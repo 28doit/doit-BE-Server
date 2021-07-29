2d2c87e8b7d9a52e5df9af75eb59c0837a2acf14
이정범 <jungbum05114@gmail.com>
2021-07-29 16:11:38 +0900

    Swagger 기본세팅

--- photo/src/main/java/com/example/photologger/photo/controller/AccountsController.java
+++ photo/src/main/java/com/example/photologger/photo/controller/AccountsController.java
@@ -2,6 +2,7 @@ package com.example.photologger.photo.controller;
 
 import com.example.photologger.photo.domain.User;
 import com.example.photologger.photo.service.AccountsService;
+import io.swagger.annotations.*;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.http.MediaType;
 import javax.servlet.http.*;
-
+/**/
 @Slf4j
 @RequestMapping("/accounts")
 @RestController
+@Api(tags= "Accounts Controller")
 public class AccountsController {
 
     private AccountsService accountsService;
@@ -28,18 +30,22 @@ public class AccountsController {
     {
         this.accountsService=accountsService;
     }
-
+/**/
     /**
      * 회원가입 페이지
      */
+    @ApiOperation(value = "회원가입 페이지", notes = "회원가입의 정보가있는 폼으로 보내준다.")
     @GetMapping("/new")
     public ModelAndView join () {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("accounts/createUserForm");
         return modelAndView;
     }
+
+    @ApiOperation(value = "회원가입", notes = "회원가입")
+
     @PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String JoIn(@RequestBody User user)
+    public String JoIn(@RequestBody @ApiParam(value = "한명의 개인정보",required = true) User user)
     {
         log.info(user.toString());
         accountsService.join(user);
@@ -47,18 +53,19 @@ public class AccountsController {
         modelAndView.setViewName("accounts/createUserForm");
         return user.toString();
     }
+    @ApiOperation(value = "로그인 페이지", notes = "로그인 정보가있는 폼으로 보내준다.")
     @GetMapping("/login")
     public String login(){
         return "user/login/login";
     }
-
+    @ApiOperation(value = "로그인", notes = "로그인.")
     @PostMapping("/login")
     public String Login()
     {
 
         return "redirect:/";
     }
-
+    @ApiOperation(value = "로그아웃", notes = "로그아웃.")
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -68,15 +75,23 @@ public class AccountsController {
         }
         return "redirect:/";
     }
+    @ApiOperation(value = "비밀번호 찾기", notes = "비밀번호 찾기")
     @PostMapping("/password-reset")
     public void PassWord_Reset()
     {
 
     }
+    @ApiOperation(value = "Id 찾기", notes = "id찾기")
     @PostMapping("/id-find")
     public String Id_Find()
     {
-        return "";
+        return "redirect:/";
+    }
+    @ApiOperation(value = "회원 삭제", notes = "회원삭제는 ??일의 보류기간을 가집니다.")
+    @PostMapping("/deleteUser")
+    public String Delete_User()
+    {
+        return "redirect:/";
     }
 
 }
