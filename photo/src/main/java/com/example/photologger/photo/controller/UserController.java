47b5f8641fbac88ca295c9373efa59ea0b37d068
Jeonghoon <ukidd12@naver.com>
2021-07-30 22:25:37 +0900

    유저 정보 불러오기 기능 추가

--- photo/src/main/java/com/example/photologger/photo/controller/UserController.java
+++ photo/src/main/java/com/example/photologger/photo/controller/UserController.java
@@ -1,13 +1,35 @@
 package com.example.photologger.photo.controller;
 
+import com.example.photologger.photo.domain.User;
 import com.example.photologger.photo.service.UserService;
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-    private UserService userService = new UserService();
+    @Autowired
+    private UserService userService;
 
+
+    // 유저 조회
+    @GetMapping("/{idx}")
+    public String findUser(@PathVariable(value = "idx") int idx)
+            throws Exception {
+        List<User> userList = userService.findOne(idx);
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+//        .addAttribute("UserList", userList);
+        return "ok";
+
+    }
 }
