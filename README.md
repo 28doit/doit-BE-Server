02e0f8d35b879bcb80c6409b9a72c0f7dcfd5134
Jeonghoon <ukidd12@naver.com>
2021-08-10 15:51:47 +0900

    fixed untracked files

--- README.md
+++
@@ -1,9 +0,0 @@
-# Spring
-###dependencies###
-
-1. Lombok [ DEVELOPER TOOLS ]
-2. Spring Web [ WEB ]
-3. MyBatis Framework [ SQL ]
-4. Thymeleaf [ TEMPLATE ENGINES ]
-
-##################
--- photo/src/main/java/com/example/photologger/photo/mapper/AccountsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.photologger.photo.mapper;
-import com.example.photologger.photo.domain.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccountsMapper {
-    void join(User user);
-}
-
